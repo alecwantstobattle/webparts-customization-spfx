69168f56dec15b5e2f4e604006f4a101fb3bcfd7
Alec Tecson <tecsonalr@gmail.com>
2021-08-20 15:47:09 +0800

    Creating No Javascript Framework SPFX Web Part

---
+++ spfx-noframework/src/webparts/psAlec/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPsAlecWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PsAlecWebPartStrings' {
+  const strings: IPsAlecWebPartStrings;
+  export = strings;
+}
