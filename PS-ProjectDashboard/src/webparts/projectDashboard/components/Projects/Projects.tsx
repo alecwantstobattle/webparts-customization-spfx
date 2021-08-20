32ea0217aeef7d2786dc924bc68a8a0c8ec3b0d8
Alec Tecson <tecsonalr@gmail.com>
2021-08-20 20:03:34 +0800

    Connecting to SharePoint REST API

--- PS-ProjectDashboard/src/webparts/projectDashboard/components/Projects/Projects.tsx
+++ PS-ProjectDashboard/src/webparts/projectDashboard/components/Projects/Projects.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { ProjectsProps } from './ProjectsProps';
-import { ProjectsState } from './ProjectsState'
+import { ProjectsState } from './ProjectsState';
+import { 
+    SPHttpClient,
+    SPHttpClientResponse,
+    ISPHttpClientOptions
+} from '@microsoft/sp-http';
 
 export class Projects extends React.Component<ProjectsProps, ProjectsState> {
     constructor(props: ProjectsProps, state: ProjectsState) {
@@ -11,7 +16,25 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState> {
     }
 
     public getItems() {
-        
+        this.props.context.spHttpClient
+        .get(
+            `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('Projects')/Items?$expand=ProjectManager&$select=*,ProjectManager/EMail,ProjectManager/Title`,
+            SPHttpClient.configurations.v1
+        )
+        .then(
+            (response: SPHttpClientResponse): Promise<{ value: any[] }> => {
+                return response.json();
+            }
+        )
+        .then(
+            (response: { value: any[] }) => {
+                var _items = [];
+                _items = _items.concat(response.value);
+                this.setState({
+                    items: _items,
+                });
+            }
+        );
     }
 
     public componentDidMount() {
@@ -20,7 +43,15 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState> {
 
     public render(): React.ReactElement<ProjectsProps> {
         return <div>
-            Hello from Projects Component
-        </div>
+            <div>
+                {this.state.items.map((item, key) => 
+                    <li key={key}>
+                        <h3>{item.Title}</h3>
+                        <div>{item.ProjectManager.Title}</div>
+                        <div>{item.Status}</div>
+                    </li>
+                )}
+            </div>
+        </div>;
     }
 }
